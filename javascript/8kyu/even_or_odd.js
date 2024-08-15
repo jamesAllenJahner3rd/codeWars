44089f6574da7f3b6ef8eb92b1a37ba18fb44b0f
James Jahner <jitone1@gmail.com>
2024-08-15 12:00:00 -0500

    even_or_odd.js

---
+++ javascript/8kyu/even_or_odd.js
@@ -0,0 +1,7 @@
+function evenOrOdd(number) {
+    //check if number has a remainder.
+    if (number%2 ===0){
+      //return whether it's even or odd.
+      return 'Even'  
+    } else return 'Odd'
+  }
