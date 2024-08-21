16ac22bc95425aff32a1dd3fc164cfce3607a0b5
James Jahner <jitone1@gmail.com>
2024-08-21 02:15:20 -0500

    grasshopper_-_Combine_strings.js

---
+++ javascript/8kyu/grasshopper_-_Combine_strings.js
@@ -0,0 +1,8 @@
+function combineNames(first,last){return `${first} ${last}`};
+
+// Combine strings function
+// Create a function named combineNames/combine_names/CombineNames that accepts two parameters (first and last name). The function should return the full name.
+
+// Example:
+
+// With "James" as the first name and "Stevens" as the last name should return "James Stevens"
