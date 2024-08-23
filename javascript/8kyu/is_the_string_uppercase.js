143b5463f453020d2395b38c5da4810b5021950c
James Jahner <jitone1@gmail.com>
2024-08-23 00:09:01 -0500

    is_the_string_uppercase.js

---
+++ javascript/8kyu/is_the_string_uppercase.js
@@ -0,0 +1,17 @@
+String.prototype.isUpperCase = function() {
+    let truthy = true;
+    for(letter of this) { if (letter >="a" && letter <="z") {truthy = false}}      
+                 return truthy
+  };
+  /**Is the string uppercase?
+Task
+Create a method to see whether the string is ALL CAPS.
+
+Examples (input -> output)
+"c" -> False
+"C" -> True
+"hello I AM DONALD" -> False
+"HELLO I AM DONALD" -> True
+"ACSKLDFJSgSKLDFJSKLDFJ" -> False
+"ACSKLDFJSGSKLDFJSKLDFJ" -> True
+In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS. */
