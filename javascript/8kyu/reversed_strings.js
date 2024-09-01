8c184e623ea1709e1d17dfb40da6332914a1acc0
James Jahner <jitone1@gmail.com>
2024-08-31 23:13:42 -0500

    reversed_strings.js

---
+++ javascript/8kyu/reversed_strings.js
@@ -0,0 +1,9 @@
+
+function solution(str){
+  return str.split('').reverse().join('')
+}
+/**
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow' */
