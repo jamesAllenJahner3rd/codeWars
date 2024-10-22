c91eb458dc40c121c942256a14a622ddad5d778e
James Jahner <jitone1@gmail.com>
2024-10-21 23:18:03 -0500

    abbreviate_a_two_word_name.js

---
+++ javascript/8kyu/abbreviate_a_two_word_name.js
@@ -0,0 +1,56 @@
+function abbrevName(name) {
+    const initials = new TheAbbreviator(name);
+    return initials.run();
+  }
+
+  class TheAbbreviator {
+    constructor(name) {
+      this._name = name;
+      this._nameArray = [];
+      this._outputArray = [];
+      this._output = "";
+    }
+
+    // Split the name into an array of words
+    arrayifier() {
+      this._nameArray = this._name.split(' ');
+    }
+
+    // Find the first letter of each word and push it to _outputArray
+    initialFinder() {
+      this._nameArray.forEach((e) => {
+        this._outputArray.push(e.charAt(0));
+      });
+    }
+
+    // Join the initials with a dot separator
+    initialize() {
+      this._output = this._outputArray.join('.');
+    }
+    // Capitalize the initials
+    capitalize(){
+      this._output = this._output.toUpperCase();
+    }
+
+    // Execute the steps to get the abbreviation
+    run() {
+      this.arrayifier();
+      this.initialFinder();
+      this.initialize();
+      this.capitalize();
+      return this._output
+    }
+  }
+
+  /*prep
+  parameter a full name
+  return the first letters of the name separated by a '.'
+
+    "Sam Harris"), "S.H");
+   "Patrick Feenan"), "P.F");
+   "Evan Cole"), "E.C");
+
+  separate the names into and array
+  get the first letter of the names.
+  add the letters to a string plus a period.
+  */
