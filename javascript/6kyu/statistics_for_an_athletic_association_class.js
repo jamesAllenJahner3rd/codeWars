b2888492feb86eea944635cd594c2b69084546cb
James Jahner <jitone1@gmail.com>
2024-12-08 21:38:36 -0600

    renamed statistics_for_an_athletic_association_class.js and created the statistics_for_an_athletic_association.js using classes

--- javascript/6kyu/statistics_for_an_athletic_association_class.js
+++ javascript/6kyu/statistics_for_an_athletic_association_class.js
@@ -1,15 +1,23 @@
+console.log("started");
 function stat(inputString) {
-  const statisticOfTimes = new StatisticFinder
+  
+  const statisticOfTimes = new StatisticFinder(inputString);
   return statisticOfTimes.start()
 }
-  if (inputString==='') return '';
+class StatisticFinder{
+  constructor(stringOfTimes){
+    this.stringOfTimes = stringOfTimes
+    this.arrayOfTimes =[];
+  }
+ unformatTimes() {
+   if (this.stringOfTimes==='') return '';
 //create a array to hold the series on numbers
-  let arrayOfTimes =[];
-  inputStringArr=inputString.split(',')
+ 
+  let inputStringArray=this.stringOfTimes.split(',')
  //change the format to seconds
-  inputStringArr.forEach((time,indexOfTime,a) =>{
+  inputStringArray.forEach((time,indexOfTime,a) =>{
     let timesBrokenDown =time.split('|');
-    arrayOfTimes.push( timesBrokenDown.reduce((acc,cur,indexOfArray,a)=>{
+    this.arrayOfTimes.push( timesBrokenDown.reduce((totalOfSeconds,cur,indexOfArray,a)=>{
       if (a[indexOfArray+1]){
         return  +totalOfSeconds*60 + +a[indexOfArray+1];
       }else{
@@ -17,27 +25,38 @@ function stat(inputString) {
       }
     },timesBrokenDown[0]))
   })
+ }
+ rangeFinder(){
   //the range is max - min
-    const maxTime = Math.max(...arrayOfTimes);
-    const minTime = Math.min(...arrayOfTimes);
+    const maxTime = Math.max(...this.arrayOfTimes);
+    const minTime = Math.min(...this.arrayOfTimes);
     const range = maxTime - minTime;
+    return range;
+  }
+  averageFinder(){
 // the average is the sum of the numbers/ the total amount of numbers
-  const sum = Math.floor(arrayOfTimes.reduce((acc,cur)=>{
-//    console.log(acc,"+",cur,arrayOfTimes.length) 
+  const sum = Math.floor(this.arrayOfTimes.reduce((acc,cur)=>{
+//    console.log(acc,"+",cur,this.arrayOfTimes.length) 
     return  acc+cur
   },0)
   ) 
-   let mean =Math.floor(sum/arrayOfTimes.length)
+   let mean =Math.floor(sum/this.arrayOfTimes.length)
+   return mean;
+}
+medianFinder(){
 //the median needs the numbers to be in order to pick the two middle numbers/2 or the middle number of there is an odd amount 
-  let median = arrayOfTimes.sort((a,b)=> a-b)
+  let median = this.arrayOfTimes.sort((a,b)=> a-b)
   if (!(median.length%2 ===0)){
   console.log("odd",median.length%2)
     median = (median[Math.floor(median.length/2)])
   }else {  
     median = (median[median.length/2]+median[median.length/2-1])/2
   }
+  return median
+}
+
   // turn the seconds back to  hh|mm|ss format
-  function timeStringify(num){
+timeFormatter(num){
     let sec = Math.floor(num%60)
     let min = Math.floor(num/60)
     let hour= Math.floor(min /60) 
@@ -46,10 +65,18 @@ function stat(inputString) {
       }
     return `${hour}`.padStart(2,0)+"|"+`${min}`.padStart(2,0)+"|"+`${sec}`.padStart(2,0)
   }
+  start(){
+    this.unformatTimes()
+    let range = this.rangeFinder()
+    let mean = this.averageFinder()
+    let median = this.medianFinder()
+    
   // return the answer in the requested form 
-  return `Range: ${timeStringify(range)} Average: ${timeStringify(mean)} Median: ${timeStringify(median)}`
+    return `Range: ${this.timeFormatter(range)} Average: ${this.timeFormatter(mean)} Median: ${this.timeFormatter(median)}`
+  }
+  
 }
-
+console.log(  stat("01|15|59, 1|47|16, 01|17|20, 1|32|34, 2|17|17"))
 /*return a string of 3 values"Range: 01|47|18 Average: 01|35|15 Median: 01|32|34"
 1|01|18
 
