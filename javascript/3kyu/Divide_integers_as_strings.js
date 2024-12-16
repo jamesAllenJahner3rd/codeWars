3e7595b4be916ef6633c1c7c32a04bdc41cc813e
KindWeb Creations <74675098+jamesAllenJahner3rd@users.noreply.github.com>
2024-12-16 13:21:33 -0600

    obfuscated Divide_integers_as_strings.js

--- javascript/3kyu/Divide_integers_as_strings.js
+++ javascript/3kyu/Divide_integers_as_strings.js
@@ -1,147 +1,103 @@
-function divideStrings(a,b) {
-  
-  
-    console.log(`${a}/${b}`)
-    
-    
-    
-    
-    aArray = [...a];
-    cArray = [];
-    rArray = [];
-    aLength = a.length-1 
-    for(let i =0; i<=aArray.length-1;i++){
-      if (i == aLength){
-        
-        cArray.push(parseInt(aArray[i]/Number(b),10))
-        rArray.push(Number(aArray[i])%Number(b))
-        
-      return [`${parseInt(cArray.join(''))}`, rArray.join('')]
-      }else  if(Number(aArray[i]) < Number(b)) {
-         cArray.push('0')
-        
-         aArray[i+1] = `${aArray[i]}${aArray[i+1] ||0}`
-         console.log(`cArray: ${cArray} aArray: ${aArray}`)
-      }else {
-        // cArray.push(parseInt(aArray[i]/Number(b),10))
-        console.log(aArray[i],Number(b))
-        while(Number(b)<= aArray[i]){
-          console.log(`${aArray[i]}-${Number(b)} = ${Number(aArray[i]) -Number(b)}`)
-          aArray[i]=Number(aArray[i]) -Number(b)
-          console.log('aArray[i]',aArray[i])
-          cArray[i]= (cArray[i]||0)+1
-          console.log('cArray[i]',cArray[i])
+function _0x1b47() {
+  const _0x5e466f = [
+    "6238968VEUSip",
+    "\x20=\x20",
+    "cArray:\x20",
+    "60060GrvQGt",
+    "log",
+    "push",
+    "369954GqMCgf",
+    "388740nhlCTK",
+    "66353sfYcBi",
+    "139897STHkNS",
+    "cArray[i]",
+    "132YkosKg",
+    "2239815iFudDq",
+    "join",
+    "length",
+  ];
+  _0x1b47 = function () {
+    return _0x5e466f;
+  };
+  return _0x1b47();
+}
+function _0x2e69(_0x9434d0, _0xed8dad) {
+  const _0x1b4739 = _0x1b47();
+  return (
+    (_0x2e69 = function (_0x2e6998, _0x16fe2e) {
+      _0x2e6998 = _0x2e6998 - 0x7f;
+      let _0x30e1ec = _0x1b4739[_0x2e6998];
+      return _0x30e1ec;
+    }),
+    _0x2e69(_0x9434d0, _0xed8dad)
+  );
+}
+(function (_0x1d71b1, _0x404668) {
+  const _0x478587 = _0x2e69,
+    _0x76189b = _0x1d71b1();
+  while (!![]) {
+    try {
+      const _0x5b656c =
+        -parseInt(_0x478587(0x80)) / 0x1 +
+        parseInt(_0x478587(0x8c)) / 0x2 +
+        -parseInt(_0x478587(0x89)) / 0x3 +
+        parseInt(_0x478587(0x8d)) / 0x4 +
+        -parseInt(_0x478587(0x83)) / 0x5 +
+        (parseInt(_0x478587(0x82)) / 0x6) * (-parseInt(_0x478587(0x7f)) / 0x7) +
+        parseInt(_0x478587(0x86)) / 0x8;
+      if (_0x5b656c === _0x404668) break;
+      else _0x76189b["push"](_0x76189b["shift"]());
+    } catch (_0xaca2e7) {
+      _0x76189b["push"](_0x76189b["shift"]());
+    }
+  }
+})(_0x1b47, 0x3bf6f);
+function divideStrings(_0x4d2eb2, _0x125098) {
+  const _0x50a4a3 = _0x2e69;
+  console["log"](_0x4d2eb2 + "/" + _0x125098),
+    (aArray = [..._0x4d2eb2]),
+    (cArray = []),
+    (rArray = []),
+    (aLength = _0x4d2eb2[_0x50a4a3(0x85)] - 0x1);
+  for (
+    let _0x3b2b5c = 0x0;
+    _0x3b2b5c <= aArray[_0x50a4a3(0x85)] - 0x1;
+    _0x3b2b5c++
+  ) {
+    if (_0x3b2b5c == aLength)
+      return (
+        cArray[_0x50a4a3(0x8b)](
+          parseInt(aArray[_0x3b2b5c] / Number(_0x125098), 0xa)
+        ),
+        rArray["push"](Number(aArray[_0x3b2b5c]) % Number(_0x125098)),
+        ["" + parseInt(cArray[_0x50a4a3(0x84)]("")), rArray["join"]("")]
+      );
+    else {
+      if (Number(aArray[_0x3b2b5c]) < Number(_0x125098))
+        cArray[_0x50a4a3(0x8b)]("0"),
+          (aArray[_0x3b2b5c + 0x1] =
+            "" + aArray[_0x3b2b5c] + (aArray[_0x3b2b5c + 0x1] || 0x0)),
+          console["log"](_0x50a4a3(0x88) + cArray + "\x20aArray:\x20" + aArray);
+      else {
+        console[_0x50a4a3(0x8a)](aArray[_0x3b2b5c], Number(_0x125098));
+        while (Number(_0x125098) <= aArray[_0x3b2b5c]) {
+          console[_0x50a4a3(0x8a)](
+            aArray[_0x3b2b5c] +
+              "-" +
+              Number(_0x125098) +
+              _0x50a4a3(0x87) +
+              (Number(aArray[_0x3b2b5c]) - Number(_0x125098))
+          ),
+            (aArray[_0x3b2b5c] = Number(aArray[_0x3b2b5c]) - Number(_0x125098)),
+            console["log"]("aArray[i]", aArray[_0x3b2b5c]),
+            (cArray[_0x3b2b5c] = (cArray[_0x3b2b5c] || 0x0) + 0x1),
+            console[_0x50a4a3(0x8a)](_0x50a4a3(0x81), cArray[_0x3b2b5c]);
         }
-        aArray[i+1] = `${aArray[i]%b}${aArray[i+1]|| 0}`
+        aArray[_0x3b2b5c + 0x1] =
+          "" +
+          (aArray[_0x3b2b5c] % _0x125098) +
+          (aArray[_0x3b2b5c + 0x1] || 0x0);
       }
     }
   }
-    
-    //console.log(parseInt(aArray[i]/Number(b)))
-    //console.log(cArray)
-    
-  // console.log(`PARAMETERS a: ${a},b:${b},aArray: ${aArray},aLength:${aLength},cArray:${cArray}`)
-  // console.log(`aArray.length: ${aArray.length}`)
-  //    for(let i =0; i<=aArray.length-1;i++){
-  // console.log('i',i)
-  // console.log("cArray",cArray)
-  // console.log(aArray[i], aArray[i]== '0', aArray[i+1]== undefined, aArray[i+1], 'so continue')
-  //       if (parseInt(aArray[i])==0 && aArray[i+1]==undefined) {
-  //        return [cArray.slice(0,aLength).join('')||'0',rArray.join('')||'0']
-  //         }else{ 
-  //           if(Number(aArray[i]) < Number(b)) {
-  // //console.log(`aArray[${i}] ${aArray[i]}<${b} is ${aArray[i]<b} so let move to the next digit`)
-  //             cArray.push('0')
-  // //console.log("aArray.length-1",aArray.length-1)
-  //             aArray[i+1] = `${aArray[i]}${aArray[i+1] ||0}`
-  // console.log(`aArray[${i+1}]is now ${aArray[i+1]}`)
-  // console.log("aArray.length-1 is now",aArray.length-1)
-  // console.log(`i+1 <= aArray.length-1 = ${ i+1}<= ${aArray.length-1}`)
-  //           }else{
-  // //console.log(`aArray[${i}]>${b} is ${aArray[i]>b} so  ${aArray[i]}/${b} = ${parseInt(aArray[i]/b)}`)
-  //             console.log("remainder",aArray[i]%b)
-  //             if (i==aLength){
-  //              rArray.push(aArray[i]%b) 
-  //              console.log(aArray[i]%b)
-  //            }
-  //             cArray.push(`${parseInt(aArray[i]/b)}`)
-  //             aArray[i+1] = `${aArray[i]%b}${aArray[i+1]|| 0}`
-  // console.log(cArray)
-  // console.log(`i+1 <= aArray.length-1 =  ${ i+1}<= ${aArray.length-1}`)
-  //           }  
-                
-    
-    
-    
-    
-    
-  //     }
-    
-  //    // return [cArray.slice(0,aLength).join('')||'0',cArray.slice(aLength).join('')||'0']
-  //    }
-  // }
-  
-  
-  
-  // function divideStrings(a,b) {
-    
-  //   aArray = [...a];
-  //   bArray = [...b];
-  //   cArray = [];
-  //   aLength = aArray.length
-  //   console.log(`PARAMETERS a: ${a},b:${b},aArray: ${aArray},bArray: ${bArray},cArray:${cArray}`)
-    
-  //   cArray = aArray.map((e,i,aA)=>{
-     
-  //     let eQuotient = Number(e)/Number (b)
-  //     console.log(` e: ${e},i:${i},a[i+1]: ${a[i+1]},eQuotient: ${eQuotient}`)
-  //     console.log(`  ________`)
-  //     console.log(`${b}/${a}`)
-      
-      
-  //     if(eQuotient <1){
-        
-  //       aA[i+1]= (aA[i+1] || '0');
-  //       console.log(`eQuotient: ${eQuotient} is less than 1, aA[i+1]: ${aA[i+1]},`)
-  //       aA[i+1]=e+aA[i+1]
-  //       console.log(`e pushes do next digit e+aA[i+1]: ${aA[i+1]},`)
-  //        console.log(`  ________`)
-  //        console.log(`${b}/${a}`)
-  //     }else {
-       
-  //        aA[i+1]= (aA[i+1]|| '0'); 
-  //       console.log(`eQuotient: ${eQuotient} is greater than 1, aA[i+1]: ${aA[i+1]}`)
-  //       console.log(`cArray[${i}] before change: ${cArray[i]} `)
-  //       console.log(`parseInt(eQuotient):${parseInt(eQuotient)} is pushed to cArray[${i}]: ${cArray[i]} `)
-  //       cArray.push(parseInt(eQuotient))
-  //       console.log(`After push cArray[${i}]: ${cArray[i]} `)
-  //       console.log("parseInt(Number(e)/Number (b)",parseInt(Number(e)/Number (b)))
-  //       aA[i+1] = `${parseInt(Number(e)/Number (b))}${(aA[i+1])}`
-  //       console.log(" aA[i+1]", aA[i+1])
-  //        console.log("cArray", cArray)
-  //     }
-  //     console.log("aA[a.length]:",aA[a.length])
-  // //     if(aA[a.length]< b){
-  //       cArray.push(aA[aLength])
-  //      console.log("cArray:",cArray)
-  // //     }
-  //   })
-  //   return [cArray.slice(0,aLength).join('')||'0',cArray.slice(a.length).join('')||'0' ]
-  // }
-    
-  
-    //return [Math.floor(+a / +b).toString(), (+a % +b).toString()];  // This doesn't work on big numbers!
-  
-  
-  //PREP
-  //A AND B ARE STRINGS OF  LARGE NUMBERS.
-  // RETURN THE QUOTIENT, AND THE REMAINDER AS AN ARRAY OF STRINGS
-  //(0, 5 ) = ['0', '0']
-  // BREAK THE PARAMETERS UP INTO ARRAYS OF STRINGS
-  // TO DO LONG DIVISION ON PAPER YOU COMPARE THE THE FIRST DIGIT OF a TO b.
-  //then return the amount of times b goes into that number. 
-  //if a is too small then you concatentate it to the next digit, repeat.
-  // then join the output. 
-  //modulus a to b to get the remainer.
-  
-  
+}
