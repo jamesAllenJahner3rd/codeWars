d2ee53dd071129fadd1c03b631ad913400ab5004
James Jahner <jitone1@gmail.com>
2024-12-19 00:51:52 -0600

    88-merge-sorted-array.js include better time

--- javascript/leetcodes/88-merge-sorted-array.js
+++ javascript/leetcodes/88-merge-sorted-array.js
@@ -29,3 +29,9 @@ return the two arrays merged
      nums1.sort((a,b)=> a-b)
 };
 merge([1,2,3,0,0,0],3,[2,5,6],3)
+// var merge = function(nums1, m, nums2, n) {
+//     for (let i = m, j = 0; j < n; i++, j++) {
+//         nums1[i] = nums2[j];
+//     }
+//     nums1.sort((a, b) => a - b);
+// };
