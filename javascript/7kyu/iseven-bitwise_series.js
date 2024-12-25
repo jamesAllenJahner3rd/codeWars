0009de02225ff3a577f2d45facccadc6ccedce20
James Jahner <jitone1@gmail.com>
2024-12-24 19:58:57 -0600

    iseven-bitwise_series.js

---
+++ javascript/7kyu/iseven-bitwise_series.js
@@ -0,0 +1,28 @@
+var isEven = function (n) {
+  console.log(n);//if n is even return true, otherwise, return false
+ while (n>0){
+   n -= 2;
+ }
+  console.log(n);
+ return n < 0 ? false: true
+}
+/*Description:
+Is the number even?
+If the numbers is even return true. If it's odd, return false.
+
+Oh yeah... the following symbols/commands have been disabled!
+
+use of %
+use of .even? in Ruby
+use of mod in Python*/
+/* prep
+parameter a number, is it an interget, whole number? float?
+return string or boolean true or false?
+2  > true
+0 > true
+3000000000000001 = false
+while > 0 subtract 2
+if the number is < 0
+ return odd
+ else return even
+*/
