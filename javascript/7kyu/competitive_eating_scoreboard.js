af318e80da985783f2235394d67cdbae35fd57db
James Jahner <jitone1@gmail.com>
2025-01-02 01:30:49 -0600

    completed competitive_eating_scoreboard

--- javascript/7kyu/competitive_eating_scoreboard.js
+++ javascript/7kyu/competitive_eating_scoreboard.js
@@ -4,7 +4,7 @@ You are the judge at a competitive eating competition and you need to choose a w
 
 There are three foods at the competition and each type of food is worth a different amount of points. Points are as follows:
 
-Chickenwings: 5 points
+chickenwings: 5 points
 
 Hamburgers: 3 points
 
@@ -62,10 +62,58 @@ Object-oriented ProgrammingFundamentals
  * 
  * 
  * 
- * 
  * pues
  * 
  * need a method to sum up the points for each food
  * need to  compare the scores
  * need to  return the object with the score and name
  */
+function scoreboard(whoAteWhat){
+    let competition = new FoodieCompetition(whoAteWhat);
+    let test = competition.tallyScores()
+    return test
+   }
+ class FoodieCompetition{
+     constructor(eatingResults){
+         this._eatingResults = eatingResults
+         let points =  new Map( [["ChickenWings", 5], ["Hamburgers", 3] ,["Hotdogs", 2]] )
+         this._points = function (){
+             return points
+         };
+         this._result =[]
+     };
+     get points(){
+         return this._points;
+         };
+     tallyScores(){
+         this.sumScores()
+         let winner = this.compareScores();
+         return winner;
+     };
+     sumScores(){
+         this._result =  this._eatingResults.map((e,i,a)=>{
+           let chickScore = e.chickenwings* this.points().get("ChickenWings")
+           let hamScore = e.hamburgers*this.points().get("Hamburgers")
+           let hotDogScore = e.hotdogs*this.points().get("Hotdogs")
+           return {
+                 "name" : e.name,
+                 "score": (chickScore + hamScore + hotDogScore)
+           }
+         })
+        
+     };
+     compareScores(){
+        return this._result.sort((a,b)=>{
+          return a.score-b.score == 0?   a.name.localeCompare(b.name): b.score - a.score
+        })
+     };
+ }
+
+
+  let whoAteWhat = [
+    {name: "Billy The Beast", chickenwings: 17 , hamburgers: 7, hotdogs: 8},
+    {name: "Habanero Hillary", chickenwings: 5 , hamburgers: 17, hotdogs: 11},
+    {name: "Joey Jaws", chickenwings: 8, hamburgers: 8, hotdogs: 15},
+    {name: "Big Bob" , chickenwings: 20, hamburgers: 4, hotdogs: 11}
+  ];
+ console.log(scoreboard(whoAteWhat))
