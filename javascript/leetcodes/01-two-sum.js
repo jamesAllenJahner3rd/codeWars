67ca7397291bf57ed937589249724f95227842cc
James Jahner <jitone1@gmail.com>
2025-01-05 00:30:50 -0600

    map of two sum

--- javascript/leetcodes/01-two-sum.js
+++ javascript/leetcodes/01-two-sum.js
@@ -19,3 +19,56 @@
 // -109 <= target <= 109
 // Only one valid answer exists.
 // Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+/**
+ * para array of integers, no letter,no punc,no float, no neg, 
+return index of the two numbers that add to the target
+ *[2,3,5,6], 9 .[1,3] 
+ * [8,5,9,6] ,13 . [0,1]
+ * [0,8,5,3], 5 [0,2]
+ * for() loop through elements
+ *  loop again through adding the element  and compare to the target
+ *  return the first one that equals the target
+ * 
+ * object map of the array elements keyed withe index
+ * subtract the element from the target 
+ * if the key exist return the index of both numbers
+ *
+ * 
+ * 
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const map = {}
+    nums.forEach((e,i,a)=>{
+        map[e] = i
+    })
+    let output =[]
+     nums.forEach((e,i,a)=>{
+        if(Object.hasOwn(map,target -e) && i !== map[target -e]){
+            output = [i, map[target - e]]
+        }
+    })
+    return output
+};
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const map = {}
+    nums.forEach((e,i,a)=>{
+        map[e] = i
+    })
+    let output =[]
+     nums.forEach((e,i,a)=>{
+        if(Object.hasOwn(map,target -e) && i !== map[target -e]){
+            output = [i, map[target - e]]
+        }
+    })
+    return output
+};
