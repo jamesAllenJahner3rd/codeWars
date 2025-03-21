34dc5049c305ee0eaede5a5313de791b523011b5
James Jahner <jitone1@gmail.com>
2025-03-20 23:47:59 -0500

    20. Valid Parentheses completed

--- javascript/leetcodes/walmart/greedy/0020-valid-parentheses.js
+++ javascript/leetcodes/walmart/greedy/0020-valid-parentheses.js
@@ -33,4 +33,65 @@ s consists of parentheses only '()[]{}'.
  * return a boolean of the correct order
  * so i could for loop through  the string or 
  * so i need to either track the char code or the chars
+ * if these is ever a closing bracket before a opening one, this should return false.
+ * i can load them into a map, with open is +1 and close is -1
+ * they are ever negative fail.
  */
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+    map = new Map([['[', 0], ['{', 0], ['(', 0]]);
+    let output = true
+    depth = 0
+    let a = 1, b = 1, c = 1;
+    for (let i = 0; i <= s.length - 1; i++) {
+        if (s[i] === '[') {
+            depth += 1;
+            a = depth;
+            map.set('[', map.get('[') + a);
+            output = false
+        }
+        if (s[i] === ']') {
+            map.set('[', map.get('[') - depth);
+            depth -= 1;
+            output = true
+        }
+        if (s[i] === '{') {
+            depth += 1;            
+            b = depth;
+            map.set('{', map.get('{') + b);
+            output = false
+        }
+        if (s[i] === '}') {
+            map.set('{', map.get('{') - depth);
+            depth -= 1;
+            output = true
+        }
+        if (s[i] === '(') {
+            depth += 1;
+            c = depth;
+            map.set('(', map.get('(') + c);
+            output = false
+            console.log(map,'(')
+        }
+        if (s[i] === ')') {
+            map.set('(', map.get('(') - depth);
+            depth -= 1;
+            console.log(map,")")
+            output = true
+        }
+console.log(map,i,"depth:", depth)
+
+        if (![...map.values()].every(x => x > -1) || Math.sign(depth)===-1 ) {
+console.log(map,i)
+            output = false
+            return output
+        }
+    }
+    if(depth!== 0){
+        return false
+    }
+    return output
+};
